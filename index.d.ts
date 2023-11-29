b1d9a246697a91ed7a6b8ab89d0615b15c21f3af
Darrell Couch <darrellneilcouch@gmail.com>
2023-11-29 11:29:55 -0600

    updated initialize type to support cors

--- index.d.ts
+++ index.d.ts
@@ -1,4 +1,8 @@
 import "reflect-metadata";
 import { Express } from "express";
 import { CorsRequest } from "cors";
-export declare const initialize: (app: Express, controllers: Array<any>, cors?: (req: CorsRequest, res: {statusCode: number | undefined; setHeader(key: string, value: string)})) => void;
+export declare const initialize: (
+    app: Express, 
+    controllers: Array<any>, 
+    cors?: (req: CorsRequest, res: { statusCode?: number | undefined; setHeader(key: string, value: string): any; end(): any; }, next: (err?: any) => any) => void
+) => void;
